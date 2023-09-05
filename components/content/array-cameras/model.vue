c9b045fd890a9320d465f9db2267b0d4546d4c5b
alvarosabu <alvaro.saburido@gmail.com>
2023-09-05 20:39:36 +0200

    feat: galaxy generator new controls

--- components/content/array-cameras/model.vue
+++ components/content/array-cameras/model.vue
@@ -3,7 +3,6 @@
 const { scene: model, animations } = await useGLTF(
   'https://raw.githubusercontent.com/Tresjs/assets/main/models/gltf/warcraft-3-alliance-footmanfanmade/source/Footman_RIG.glb',
 )
-console.log('jaime ~ model:', model)
 const { actions, mixer } = useAnimations(animations, model)
 
 let currentAction = actions.Idle
@@ -34,31 +33,6 @@ watch(animation, value => {
   currentAction.play()
 })
 
-/* const { pane } = useTweakPane()
-
-const animationList = pane.addBlade({
-  view: 'list',
-  label: 'Animations',
-  options: [
-    { text: 'Idle', value: 'Idle' },
-    { text: 'SwordAndShieldCrouchBlockIdle', value: 'SwordAndShieldCrouchBlockIdle' },
-    { text: 'SwordAndShieldDeath_2', value: 'SwordAndShieldDeath_2' },
-    { text: 'SwordAndShieldIdle', value: 'SwordAndShieldIdle' },
-    { text: 'SwordAndShieldKick', value: 'SwordAndShieldKick' },
-    { text: 'SwordAndShieldRun(back)', value: 'SwordAndShieldRun(back)' },
-    { text: 'SwordAndShieldRun', value: 'SwordAndShieldRun' },
-    { text: 'SwordAndShieldSlash_2', value: 'SwordAndShieldSlash_2' },
-    { text: 'SwordAndShieldSlash', value: 'SwordAndShieldSlash' },
-    { text: 'T-Pose', value: 'T-Pose' },
-  ],
-  value: 'Idle',
-})
-
-animationList.on('change', value => {
-  currentAction.stop()
-  currentAction = actions[value.value]
-  currentAction.play()
-}) */
 
 const { onLoop } = useRenderLoop()
 
