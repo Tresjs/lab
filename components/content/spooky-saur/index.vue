73b27b9976896788fd021bd3cda20dc33fd4d2ba
alvarosabu <alvaro.saburido@gmail.com>
2023-10-17 14:26:36 +0200

    feat: halloween spooky-saur

--- components/content/spooky-saur/index.vue
+++ components/content/spooky-saur/index.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import type { Object3D } from 'three'
-import { BasicShadowMap, SRGBColorSpace, NoToneMapping, DoubleSide, Vector3, PCFSoftShadowMap } from 'three'
-import { vLightHelper, vAlwaysLookAt } from '@tresjs/cientos'
+import type { Object3D, PerspectiveCamera } from 'three'
+import { SRGBColorSpace, NoToneMapping, DoubleSide, Vector3, PCFSoftShadowMap } from 'three'
+import { vLightHelper } from '@tresjs/cientos'
 
 const gl = {
   clearColor: '#A590FF',
   shadows: true,
-  alpha: false,
+  alpha: true,
   shadowMapType: PCFSoftShadowMap,
   outputColorSpace: SRGBColorSpace,
   toneMapping: NoToneMapping,
 }
+
+useControls('fpsgraph')
 
 const { value: position } = useControls({
   position: new Vector3(0, 1.5, -0.3),
@@ -50,25 +52,60 @@ const bloomParams = reactive({
   intensity: 0.1,
 })
 
+const cameraRef = ref<PerspectiveCamera | null>(null)
 const insideLightRef = ref<Object3D | null>(null)
+const fontPath = '/fonts/jetbrains-mono.json'
+
 const { onLoop } = useRenderLoop()
 
 onLoop(({ elapsed }) => {
   if (insideLightRef.value) {
     insideLightRef.value.intensity = Math.sin(elapsed) * 6 + 7
   }
+  if (cameraRef.value) {
+    cameraRef.value.lookAt(0, 1, 0)
+  }
 })
+
+const { hasFinishLoading, progress } = await useProgress()
 </script>
 
 <template>
-  <TresLeches />
+  <Transition
+    name="fade-overlay"
+    enter-active-class="opacity-1 transition-opacity duration-200"
+    leave-active-class="opacity-0 transition-opacity duration-200"
+  >
+    <div
+      v-show="!hasFinishLoading"
+      class="absolute bg-[#A590FF] t-0 l-0 w-full h-full z-20 flex justify-center items-center text-black font-mono"
+    >
+      <div class="w-200px text-black text-center">
+        <p class="animate-tada">
+          🎃🦇
+        </p>
+        Loading... {{ progress }} %
+      </div>
+    </div>
+  </Transition>
   <TresCanvas v-bind="gl">
-    <TresPerspectiveCamera :position="[3, 3, 3]" />
-    <OrbitControls />
+    <TresPerspectiveCamera
+      ref="cameraRef"
+      :look-at="[0, 1, 0]"
+      :position="[1, 1, 8]"
+    />
+    <OrbitControls
+      :max-distance="7"
+      :min-azimuth-angle="-Math.PI / 6"
+      :max-azimuth-angle="Math.PI / 6"
+      :min-polar-angle="0"
+      :max-polar-angle="Math.PI / 2 - Math.PI / 18" 
+    />
     <Backdrop
       :floor="10"
-      :scale="[100, 100, 10]"
-      :position="[0, -0.15, -20]"
+      :scale="[150, 40, 10]"
+      :position="[0, -0.05, -20]"
+      
       receive-shadow
     >
       <TresMeshStandardMaterial
@@ -76,6 +113,17 @@ onLoop(({ elapsed }) => {
         :side="DoubleSide"
       />
     </Backdrop>
+    <Suspense>
+      <Text3D
+        text="SPOOKY"
+        :font="fontPath"
+        :size="2"
+        :position="[-2, 4, -6]"
+        :rotation="[0, Math.PI / 3, 0]"
+      >
+        <TresMeshStandardMaterial />
+      </Text3D>
+    </Suspense>
     <TresPointLight
       ref="insideLightRef"
       color="#FF3F00"
@@ -96,7 +144,11 @@ onLoop(({ elapsed }) => {
       :shadow-bias="-0.000001"
       cast-shadow
     />
-
+    <TresFog
+      :color="gl.clearColor"
+      :near="2"
+      :far="20"
+    />
     <Suspense>
       <PumpkinIvysaur />
     </Suspense>
@@ -108,9 +160,8 @@ onLoop(({ elapsed }) => {
     </EffectComposer>
     <TresAmbientLight :intensity="0.1" />
     <TresDirectionalLight
-      v-light-helper
       color="#FF3F00"
-      :intensity="0.2"
+      :intensity="0.6"
       :position="[3, 3, 3]"
       :shadow-mapSize-width="1024"
       :shadow-mapSize-height="1024"
@@ -119,7 +170,21 @@ onLoop(({ elapsed }) => {
       :shadow-camera-right="10"
       :shadow-camera-top="10"
       :shadow-camera-bottom="-10"
+      :shadow-bias="-0.000001"
       cast-shadow
+    />
+    <TresDirectionalLight
+      color="white"
+      :intensity="0.4"
+      :position="[2, 1, 3]"
+      :shadow-mapSize-width="1024"
+      :shadow-mapSize-height="1024"
+      :shadow-camera-far="50"
+      :shadow-camera-left="-10"
+      :shadow-camera-right="10"
+      :shadow-camera-top="10"
+      :shadow-camera-bottom="-10"
+      :shadow-bias="-0.000001"
     />
   </TresCanvas>
 </template>
