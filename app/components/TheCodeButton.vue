ddf13ede55ca92caaf3550de56201b96215a4d7b
alvarosabu <alvaro.saburido@gmail.com>
2025-08-05 17:04:08 +0200

    feat: add toggle functionality to TheCodeButton component
    - Introduced a visibility toggle for the `TheCodeButton` component using keyboard shortcuts. The button is now conditionally rendered based on the `visible` state, which can be toggled with the 'h' key.
    - This enhancement improves user interaction by allowing users to show or hide the button dynamically.

--- app/components/TheCodeButton.vue
+++ app/components/TheCodeButton.vue
@@ -3,9 +3,17 @@ defineProps<{
   title: string
   to: string
 }>()
+
+const visible = ref(true)
+
+defineShortcuts({
+  h: () => {
+    visible.value = !visible.value
+  }
+})
 </script>
 
 <template>
-  <UButton :to="to" :title="title" icon="i-carbon-logo-github" class="rounded-full" color="neutral" variant="subtle"
-    size="sm" />
+  <UButton v-if="visible" :to="to" :title="title" icon="i-carbon-logo-github" class="rounded-full" color="neutral"
+    variant="subtle" size="sm" />
 </template>
